b796736118e8479bcb6d78212ac7e399bd5a2659
Kyle Geske <stungeye@gmail.com>
2018-02-02 16:12:24 -0600

    Added the Faker gem and then used it in the db/seeds.rb script to generate a bunch of realistic data for testing the app.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+1000.times do
+  Product.create(
+      title: Faker::Commerce.unique.product_name,
+      description: "Surprisingly this product is made out of pure #{Faker::Commerce.material}.",
+      price: Faker::Commerce.price,
+      stock_quantity: Faker::Number.number(3))
+end
+
+puts "Created #{Product.count} products."
